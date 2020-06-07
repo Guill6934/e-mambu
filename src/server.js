d255c755f86693f3daa9264ca35f48204cb4ef6c
gsahonero <gsahonero@users.noreply.github.com>
2020-06-07 03:45:57 -0400

    Hello World

--- src/server.js
+++ src/server.js
@@ -7,6 +7,14 @@ const app = express();
 
 app.set('port', process.env.PORT || 3000)
 
+
+// routes
+app.get('/', () => {
+  res.send('Hello World');
+});
+
+
+// start the server
 app.listen(app.get('port'), function () {
   console.log('server on port ', app.get('port'))
 })
